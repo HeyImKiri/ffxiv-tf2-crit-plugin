3cf41b18cf2cd0ebb8d0cd820ca033ba0d1abc2a
HeyImKiri <emailingkiriakashi@gmail.com>
2026-04-29 18:23:15 -0500

    API 15
    Guard action lookup against missing ClassJob entries
    
    - Use TryGetValue when reading parent job actions
    - Ensure dictionary keys exist before adding actions
    - Prevent crash when a job has no associated actions in Action sheet

--- Tf2CriticalHitsPlugin/Constants.cs
+++ Tf2CriticalHitsPlugin/Constants.cs
@@ -45,67 +45,79 @@ public abstract class Constants
         return jobList.ToImmutableSortedDictionary(j => j.RowId, j => j);
     }
 
-    private static ImmutableDictionary<uint, ISet<string>> InitActionPerJob()
+private static ImmutableDictionary<uint, ISet<string>> InitActionPerJob()
+{
+    if (Service.DataManager == null) throw new ApplicationException("DataManager not initialized!");
+    var result = new Dictionary<uint, ISet<string>>();
+    var actionSheet = Service.DataManager.GetExcelSheet<Action>();
+    if (actionSheet is null) throw new ApplicationException("Action sheet unavailable!");
+
+    foreach (var action in actionSheet)
     {
-        if (Service.DataManager == null) throw new ApplicationException("DataManager not initialized!");
-        var result = new Dictionary<uint, ISet<string>>();
-        var actionSheet = Service.DataManager.GetExcelSheet<Action>();
-        if (actionSheet is null) throw new ApplicationException("Action sheet unavailable!");
-        foreach (var action in actionSheet)
+        if (!result.ContainsKey(action.ClassJob.RowId))
         {
-            if (!result.ContainsKey(action.ClassJob.RowId))
-            {
-                result[action.ClassJob.RowId] = new HashSet<string>();
-            }
-
-            result[action.ClassJob.RowId].Add(action.Name.ExtractText());
+            result[action.ClassJob.RowId] = new HashSet<string>();
         }
 
-        var classJobSheet = Service.DataManager.GetExcelSheet<ClassJob>();
-        
-        // Add the parent Class's action to a Job action
-        // (for when your White Mage *insists* on using Cure I)
-        foreach (var classJob in classJobSheet!)
-        {
-            if (classJob.ClassJobParent.Value.RowId != classJob.RowId)
-            {
-                foreach (var parentAction in result[classJob.ClassJobParent.Value.RowId])
-                {
-                    if (!result.ContainsKey(classJob.RowId))
-                    {
-                        result[classJob.RowId] = new HashSet<string>();
-                    }
-                    result[classJob.RowId].Add(parentAction);
-                }
-            }
-        }
-        
-        // Second Wind special case 🌈
-        var secondWind = actionSheet.GetRowOrDefault(57);
-        if (secondWind is not null)
-        {
-            foreach (var classJob in classJobSheet.Where(c => c.Role is 2 or 3))
-            {
-                result[classJob.RowId].Add(secondWind.Value.Name.ExtractText());
-            }
-        }
-        
-        // Bloodbath super special case 🌈
-        var bloodbath = actionSheet.GetRowOrDefault(34);
-        if (bloodbath is not null)
-        {
-            // Role 3 includes magical DPS, but eh
-            foreach (var classJob in classJobSheet.Where(c => c.Role is 3))
-            {
-                result[classJob.RowId].Add(bloodbath.Value.Name.ExtractText());
-            }
-        }
-
-        foreach (var (key, value) in result)
-        {
-            result[key] = value.ToImmutableHashSet();
-        }
-
-        return result.ToImmutableDictionary();
+        result[action.ClassJob.RowId].Add(action.Name.ExtractText());
     }
+
+    var classJobSheet = Service.DataManager.GetExcelSheet<ClassJob>();
+    
+    // Add the parent Class's action to a Job action
+    foreach (var classJob in classJobSheet!)
+    {
+        var parentId = classJob.ClassJobParent.RowId;
+
+        if (parentId != classJob.RowId && result.TryGetValue(parentId, out var parentActions))
+        {
+            if (!result.ContainsKey(classJob.RowId))
+            {
+                result[classJob.RowId] = new HashSet<string>();
+            }
+
+            foreach (var parentAction in parentActions)
+            {
+                result[classJob.RowId].Add(parentAction);
+            }
+        }
+    }
+    
+    // Second Wind special case 🌈
+    var secondWind = actionSheet.GetRowOrDefault(57);
+    if (secondWind is not null)
+    {
+        foreach (var classJob in classJobSheet.Where(c => c.Role is 2 or 3))
+        {
+            if (!result.ContainsKey(classJob.RowId))
+            {
+                result[classJob.RowId] = new HashSet<string>();
+            }
+
+            result[classJob.RowId].Add(secondWind.Value.Name.ExtractText());
+        }
+    }
+    
+    // Bloodbath super special case 🌈
+    var bloodbath = actionSheet.GetRowOrDefault(34);
+    if (bloodbath is not null)
+    {
+        foreach (var classJob in classJobSheet.Where(c => c.Role is 3))
+        {
+            if (!result.ContainsKey(classJob.RowId))
+            {
+                result[classJob.RowId] = new HashSet<string>();
+            }
+
+            result[classJob.RowId].Add(bloodbath.Value.Name.ExtractText());
+        }
+    }
+
+    foreach (var (key, value) in result.ToList())
+    {
+        result[key] = value.ToImmutableHashSet();
+    }
+
+    return result.ToImmutableDictionary();
+}
 }
